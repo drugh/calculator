48eef425535d0a7f9277044d485eb0c6214314af
drugh <darklightme73@gmail.com>
2020-11-18 22:26:28 +0200

    looks nice, does multiple operations

--- script.js
+++ script.js
@@ -1,5 +1,82 @@
 // CALCULATOR PROJECT by Vlada Mihai Catalin
 
+// Getting the DOM elements
+const container = document.querySelector('.container');
+const numbers = document.querySelectorAll('.number');
+const plus = document.getElementById('add');
+const minus = document.getElementById('subtract');
+const star = document.getElementById('multiply');
+const slash = document.getElementById('divide');
+const equal = document.getElementById('equal');
+const display = document.getElementById('display');
+
+let operator = '';
+let first;
+let second = 0;
+let isEvaluated = false;
+
+numbers.forEach( number => {
+    number.addEventListener('click', (e) => {
+        second = 10*second + Number(e.target.textContent);
+        display.textContent += second%10;
+        isEvaluated = false;
+    });
+});
+
+plus.addEventListener('click', (e) => {
+    if(!isEvaluated && first) {
+        first = operate(operator, first, second);
+    }
+    if (!first) {
+        first = second;
+    }
+    operator = e.target.textContent;
+    display.textContent += operator;
+    second = 0;
+    isEvaluated = true;
+});
+minus.addEventListener('click', (e) => {
+    if(!isEvaluated && first) {
+        first = operate(operator, first, second);
+    }
+    if (!first) {
+        first = second;
+    }
+    operator = e.target.textContent;
+    display.textContent += operator;
+    second = 0;
+    isEvaluated = true;
+});
+star.addEventListener('click', (e) => {
+    if(!isEvaluated && first) {
+        first = operate(operator, first, second);
+    }
+    if (!first) {
+        first = second;
+    }
+    operator = e.target.textContent;
+    display.textContent += operator;
+    second = 0;
+    isEvaluated = true;
+});
+slash.addEventListener('click', (e) => {
+    if(!isEvaluated && first) {
+        first = operate(operator, first, second);
+    }
+    if (!first) {
+        first = second;
+    }
+    operator = e.target.textContent;
+    display.textContent += operator;
+    second = 0;
+    isEvaluated = true;
+});
+
+equal.addEventListener('click', () => {
+    display.textContent = operate(operator, first, second);
+    first = operate(operator, first, second);
+    isEvaluated = true;
+})
 
 
 // FUNCTIONS OF A CALCULATOR ------>
@@ -13,12 +90,11 @@ function multiply(a, b) {
     return a*b;
 }
 function divide(a, b) {
-    return (a/b);
+    return Math.floor(a/b * 100) / 100;
 }
 // <--------------
 
 // DYNAMICALLY CALLS AN OPERATOR ------>
-let operator = '+';
 function operate(operator, a, b) {
     switch (operator) {
         case '+':
@@ -34,7 +110,7 @@ function operate(operator, a, b) {
             return divide(a, b);
             break;
         default:
-            console.log('Not a normal operator!');
+            return 0;
             break;
     }
 }
