da3229eccccb97d3d113fe03afcc805cccbdb1a6
drugh <darklightme73@gmail.com>
2020-11-20 10:45:03 +0200

    better functionality, not all buttons work

--- script.js
+++ script.js
@@ -7,90 +7,149 @@ const plus = document.getElementById('add');
 const minus = document.getElementById('subtract');
 const star = document.getElementById('multiply');
 const slash = document.getElementById('divide');
+const dot = document.getElementById('dot');
 const equal = document.getElementById('equal');
 const display = document.getElementById('display');
+const decimals = document.getElementById('decimals');
+const clear = document.getElementById('clear');
 
-let operator = '';
-let first;
-let second = 0;
-let isEvaluated = false;
+display.textContent = '0';
+let operator = '+';
+let first = '0';
+let second = '';
+let result = null;
 
+
+
+//  FUNCTIONALITY ===========>
+
+
+// SET THE NUMBER OF DECIMALS TO BE DISPLAYED
+//   CAN ONLY GET VALUES BETWEEN 0 AND 5
+let numberOfDecimals = 0;
+decimals.addEventListener('click', (e) => {
+    if (Number(decimals.firstChild.textContent) === 5) {
+        decimals.firstChild.textContent = -1;
+    }
+    decimals.firstChild.textContent = Number(decimals.firstChild.textContent) + 1;
+    numberOfDecimals = Number(decimals.firstChild.textContent);
+});
+
+clear.addEventListener('click', (e) => {
+    clearScreen();
+});
+
+//  EACH NUMBER YOU ENTER IS STORED IN THE VARIABLE "second"
 numbers.forEach( number => {
     number.addEventListener('click', (e) => {
-        second = 10*second + Number(e.target.textContent);
-        display.textContent += second%10;
-        isEvaluated = false;
+        second += e.target.textContent;
+        console.log(first);
+        console.log(operator);
+        console.log(second);
+        displayText(second);
     });
 });
 
 plus.addEventListener('click', (e) => {
-    if(!isEvaluated && first) {
-        first = operate(operator, first, second);
-    }
-    if (!first) {
-        first = second;
-    }
-    operator = e.target.textContent;
-    display.textContent += operator;
-    second = 0;
-    isEvaluated = true;
+    result = operate(operator, first, second);
+    first = result;
+    operator = '+';
+    second = '';
+    console.log(first);
+    console.log(operator);
+    console.log(second);
+    displayText(result + operator);
 });
 minus.addEventListener('click', (e) => {
-    if(!isEvaluated && first) {
-        first = operate(operator, first, second);
-    }
-    if (!first) {
-        first = second;
-    }
-    operator = e.target.textContent;
-    display.textContent += operator;
-    second = 0;
-    isEvaluated = true;
+    result = operate(operator, first, second);
+    first = result;
+    operator = '-';
+    second = '';
+    console.log(first);
+    console.log(operator);
+    console.log(second);
+    displayText(result + operator);
 });
 star.addEventListener('click', (e) => {
-    if(!isEvaluated && first) {
-        first = operate(operator, first, second);
-    }
-    if (!first) {
-        first = second;
-    }
-    operator = e.target.textContent;
-    display.textContent += operator;
-    second = 0;
-    isEvaluated = true;
+    result = operate(operator, first, second);
+    first = result;
+    operator = '*';
+    second = '';
+    console.log(first);
+    console.log(operator);
+    console.log(second);
+    displayText(result + operator);
 });
 slash.addEventListener('click', (e) => {
-    if(!isEvaluated && first) {
-        first = operate(operator, first, second);
-    }
-    if (!first) {
-        first = second;
-    }
-    operator = e.target.textContent;
-    display.textContent += operator;
-    second = 0;
-    isEvaluated = true;
+    result = operate(operator, first, second);
+    first = result;
+    operator = '/';
+    second = '';
+    console.log(first);
+    console.log(operator);
+    console.log(second);
+    displayText(result + operator);
 });
 
 equal.addEventListener('click', () => {
-    display.textContent = operate(operator, first, second);
-    first = operate(operator, first, second);
-    isEvaluated = true;
-})
+    result = operate(operator, first, second);
+    console.log(first);
+    console.log(operator);
+    console.log(second);
+    displayText(result);
+    if (operate(operator, first, second) === 'ERROR') {
+        clearScreen('ERROR');
+    }
+});
 
+dot.addEventListener('click', (e) => {
+    // loop through each character of pullString and if a dot appears, you can't add it again
+    // this needs to look after an operator, not the whole string
+    // display.textContent = e.target.textContent;
+});
+// <=============
+
+
+
+//  FUNCTIONS ============>
+
+function displayText(string) {
+    if (result === null) {
+        result = 0;
+    }
+    display.textContent = string;
+}
+
+function clearScreen(err) {
+    operator = '+';
+    first = '0';
+    second = '';
+    result = null;
+    if (err === 'ERROR') {
+        return;
+    } else {
+        display.textContent = '0';
+    }
+}
 
 // FUNCTIONS OF A CALCULATOR ------>
 function add(a, b) {
-    return a+b;
+    return Number(a)+Number(b);
 }
 function subtract(a, b) {
-    return a-b;
+    return Number(a)-Number(b);
 }
 function multiply(a, b) {
-    return a*b;
+    if (!Number(a) || !Number(b)) {
+        return 'ERROR';
+    }
+    return Number(a)*Number(b);
 }
 function divide(a, b) {
-    return Math.floor(a/b * 100) / 100;
+    if (b == 0) {
+        return 'ERROR';
+    }
+    return Math.floor(Number(a)/Number(b) * (10**numberOfDecimals)) / (10**numberOfDecimals);
 }
 // <--------------
 
@@ -110,8 +169,9 @@ function operate(operator, a, b) {
             return divide(a, b);
             break;
         default:
-            return 0;
+            return 'ERROR';
             break;
     }
 }
 // <----------------
+// <================
